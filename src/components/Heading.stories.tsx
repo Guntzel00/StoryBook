9b5cb2002d1ab5e3048056be481a1982336d62a6
Gustavo Guntzel <gustavo.g.s01@hotmail.com>
2022-10-14 12:58:57 -0300

    First two components from or styleguide

---
+++ src/components/Heading.stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading, HeadingProps } from './Heading';
+
+export default {
+	title: 'Compoonents/Heading',
+	component: Heading,
+	args: {
+		children: 'Lorem Ipsum',
+		size: 'md',
+	},
+	argTypes: {
+		size: {
+			options: ['sm', 'md', 'lg'],
+			control: {
+				type: 'inline-radio',
+			},
+		},
+	},
+} as Meta<HeadingProps>;
+
+export const Default: StoryObj<HeadingProps> = {};
+
+export const Small: StoryObj<HeadingProps> = {
+	args: {
+		size: 'sm',
+	},
+};
+export const Large: StoryObj<HeadingProps> = {
+	args: {
+		size: 'lg',
+	},
+};
+export const customComponent: StoryObj<HeadingProps> = {
+	args: {
+		asChild: true,
+		children: <p>Testando</p>,
+	},
+	argTypes: {
+		children: {
+			table: {
+				disable: true,
+			},
+		},
+	},
+};
---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './text';
+
+export default {
+	title: 'Compoonents/Text',
+	component: Text,
+	args: {
+		children: 'Lorem Ipsum',
+	},
+	argTypes: {
+		size: {
+			defaultValue: 'md',
+			options: ['sm', 'md', 'lg'],
+			control: {
+				type: 'inline-radio',
+			},
+		},
+	},
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {};
+
+export const Small: StoryObj<TextProps> = {
+	args: {
+		size: 'sm',
+	},
+};
+export const Large: StoryObj<TextProps> = {
+	args: {
+		size: 'lg',
+	},
+};
+export const customComponent: StoryObj<TextProps> = {
+	args: {
+		asChild: true,
+		children: <p>Testando</p>,
+	},
+	argTypes: {
+		children: {
+			table: {
+				disable: true,
+			},
+		},
+	},
+};
